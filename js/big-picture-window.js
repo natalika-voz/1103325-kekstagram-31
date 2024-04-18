e40d57f176168237b061d85fffc9d2d3830e231b
natalika-voz <kochyrovanatalia@gmail.com>
2024-04-18 13:33:15 +0500

    Add module with full-size image

--- js/big-picture-window.js
+++ js/big-picture-window.js
@@ -1,14 +1,43 @@
-
 const modalEl = document.querySelector('.big-picture');
-const picturesContainerEl = document.querySelector('.pictures');
 const closeModalEl = modalEl.querySelector('.big-picture__cancel');
+
+export function createBigPicture({ url, likes, comments, description }) {
+  const imgEl = modalEl.querySelector('.big-picture__img').querySelector('img');
+  const likesCount = modalEl.querySelector('.likes-count');
+  const socialComments = modalEl.querySelector('.social__comments');
+  const socialCommentTemplate = socialComments.querySelector('.social__comment');
+  const commentsCaption = modalEl.querySelector('.social__caption');
+  const commentsCount = modalEl.querySelector('.social__comment-count');
+  const commentsLoader = modalEl.querySelector('.social__comments-loader');
+
+  const socialCommentsFragment = document.createDocumentFragment();
+
+  imgEl.src = url;
+  likesCount.textContent = likes;
+  socialComments.innerHTML = '';
+
+  comments.forEach((comment) => {
+    const socialComment = socialCommentTemplate.cloneNode(true);
+
+    socialComment.querySelector('.social__picture').src = comment.avatar;
+    socialComment.querySelector('.social__picture').alt = comment.name;
+    socialComment.querySelector('.social__text').textContent = comment.message;
+
+    socialCommentsFragment.appendChild(socialComment);
+  });
+
+  socialComments.appendChild(socialCommentsFragment);
+  commentsCaption.textContent = description;
+  commentsCount.classList.add('hidden');
+  commentsLoader.classList.add('hidden');
+}
 
 function closeModal() {
   modalEl.classList.add('hidden');
   document.body.classList.remove('modal-open');
 }
 
-function openModal() {
+export function openModal() {
   modalEl.classList.remove('hidden');
   document.body.classList.add('modal-open');
 
@@ -22,10 +51,3 @@ function openModal() {
     }
   });
 }
-
-picturesContainerEl.addEventListener('click', (evt) => {
-  evt.preventDefault();
-  console.log(evt.target);
-  openModal();
-});
-// Окно должно открываться при клике на миниатюру. Данные для окна (изображение, комментарии, лайки и так далее) берите из того же объекта, который использовался для отрисовки соответствующей миниатюры.
--- js/main.js
+++ js/main.js
@@ -1,5 +1,21 @@
 import { generateMockPosts } from './data.js';
-import { buildThumbnails } from './thumbnails.js';
+import { createThumbnails } from './thumbnails.js';
+import { createBigPicture, openModal } from './big-picture-window.js';
 
 const thumbnails = generateMockPosts();
-buildThumbnails(thumbnails);
+const picturesContainerEl = document.querySelector('.pictures');
+// вызов функции с аргументом
+createThumbnails(thumbnails);
+
+picturesContainerEl.addEventListener('click', (evt) => {
+  const currentThumbnailEl = evt.target.closest('.picture');
+  const pictureId = currentThumbnailEl.dataset.pictureId;
+
+  const currentThumbnailData = thumbnails.find((thumbnail) => thumbnail.id === Number(pictureId));
+
+  if (currentThumbnailEl) {
+    evt.preventDefault();
+    createBigPicture(currentThumbnailData);
+    openModal();
+  }
+});
