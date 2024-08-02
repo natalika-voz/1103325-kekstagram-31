d9b0652865645e0f0c2f470496e377a51778959f
natalika-voz <kochyrovanatalia@gmail.com>
2024-08-02 15:27:51 +0500

    Add post request

--- js/api.js
+++ js/api.js
@@ -1,4 +1,17 @@
+// const url = 'https://31.javascript.htmlacademy.pro/kekstagram';
+const url = 'https://31.jjjjjavascript.htmlacademy.pro/kekstagram';
+
 export function fetchPosts() {
-  return fetch('https://31.javascript.htmlacademy.pro/kekstagram/data')
+  return fetch(`${url}/data`)
     .then((response) => response.json());
+}
+
+export function createPost(data) {
+  return fetch(url, {
+    method: 'POST',
+    body: JSON.stringify(data),
+    headers: {
+      'Content-Type': 'multipart/form-data',
+    },
+  });
 }
