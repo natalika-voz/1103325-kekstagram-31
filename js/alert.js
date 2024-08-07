e4ea098e3a2ea63b49b7e06a63c5b76f3cb9ce00
natalika-voz <kochyrovanatalia@gmail.com>
2024-08-07 15:11:16 +0500

    Update alerts

--- js/alert.js
+++ js/alert.js
@@ -1,15 +1,49 @@
 function buildPostsLoadError(container) {
   const template = document.querySelector('#posts-load-error').content;
-  const error = template.querySelector('.data-error');
-  container.appendChild(error);
+  const el = template.querySelector('.data-error');
+  container.appendChild(el);
 
-  return error;
+  return el;
 }
 
-export function showError() {
-  const error = buildPostsLoadError(document.body);
+function buildSubmitPostError(container) {
+  const template = document.querySelector('#image-load-error').content;
+  const el = template.querySelector('.error');
+  container.appendChild(el);
+
+  return el;
+}
+
+function buildSubmitPostSuccess(container) {
+  const template = document.querySelector('#image-load-success').content;
+  const el = template.querySelector('.success');
+  container.appendChild(el);
+
+  return el;
+}
+
+export function showLoadPostsError() {
+  const el = buildPostsLoadError(document.body);
 
   window.setTimeout(() => {
-    error.remove();
+    el.remove();
   }, 5000);
 }
+
+export function showSubmitPostError() {
+  const el = buildSubmitPostError(document.body);
+
+  window.setTimeout(() => {
+    el.remove();
+  }, 5000);
+}
+
+export function showSubmitPostSuccess() {
+  const el = buildSubmitPostSuccess(document.body);
+
+  window.setTimeout(() => {
+    el.remove();
+  }, 5000);
+}
+
+// Нужно сделать общую ошибку "Что-то пошло не так"
