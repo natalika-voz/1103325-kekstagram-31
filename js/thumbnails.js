fc002ec2addd16452c93d63134b2a4d9f9c404d5
natalika-voz <kochyrovanatalia@gmail.com>
2024-08-07 11:56:22 +0500

    Refactoring thumbnail module

--- js/thumbnails.js
+++ js/thumbnails.js
@@ -1,22 +1,27 @@
-const buildThumbnails = (thumbnails) => {
+function createThumbnail({ url, description, likes, comments, id }, picture) {
+  const pictureElement = picture.cloneNode(true);
+  const img = pictureElement.querySelector('.picture__img');
+
+  img.src = url;
+  img.alt = description;
+  pictureElement.querySelector('.picture__likes').textContent = likes;
+  pictureElement.querySelector('.picture__comments').textContent = comments.length;
+  pictureElement.dataset.pictureId = id;
+
+  return pictureElement;
+}
+
+function buildThumbnails(thumbnails) {
   const pictures = document.querySelector('.pictures');
-  const picture = document.querySelector('#picture').content.querySelector('.picture');
+  const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
   const fragment = document.createDocumentFragment();
 
-  thumbnails.forEach(({ url, comments, description, likes, id }) => {
-    const pictureElement = picture.cloneNode(true);
-    const img = pictureElement.querySelector('.picture__img');
-
-    img.src = url;
-    img.alt = description;
-    pictureElement.querySelector('.picture__likes').textContent = likes;
-    pictureElement.querySelector('.picture__comments').textContent = comments.length;
-    pictureElement.dataset.pictureId = id;
-
+  thumbnails.forEach((data) => {
+    const pictureElement = createThumbnail(data, pictureTemplate);
     fragment.appendChild(pictureElement);
   });
 
   pictures.appendChild(fragment);
-};
+}
 
 export { buildThumbnails };
