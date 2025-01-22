5ac9f47d42f3f3a77582b180d471e712b51cd603
natalika-voz <kochyrovanatalia@gmail.com>
2025-01-22 21:20:57 +0500

    Close the modal outside and press Esc

--- js/big-picture-modal/index.js
+++ js/big-picture-modal/index.js
@@ -1,35 +1,59 @@
+// Этот код реализует модальное окно для просмотра большого изображения с детальной информацией
+
 import { buildBigPicture } from './big-picture';
+import { isEscapeKey } from '../utils';
 
 const picturesContainerEl = document.querySelector('.pictures');
 const bigPictureEl = document.querySelector('.big-picture');
 
+function show(evt, data) {
+  const currentThumbnailEl = evt.target.closest('.picture');
+
+  if (!currentThumbnailEl) {
+    return;
+  }
+
+  const pictureId = currentThumbnailEl.dataset.pictureId;
+  const currentThumbnailData = data.find((thumbnail) => thumbnail.id === Number(pictureId));
+
+  if (!currentThumbnailEl) {
+    return;
+  }
+
+  evt.preventDefault();
+  buildBigPicture(bigPictureEl, currentThumbnailData);
+  bigPictureEl.classList.remove('hidden');
+  document.body.classList.add('modal-open');
+
+  addListeners();
+}
+
+function close() {
+  bigPictureEl.classList.add('hidden');
+  document.body.classList.remove('modal-open');
+  removeListeners();
+}
+
+function addListeners() {
+  const closeEl = bigPictureEl.querySelector('.cancel');
+  document.addEventListener('keydown', onEscapeClose);
+  closeEl.addEventListener('click', close);
+}
+
+function removeListeners() {
+  const closeEl = bigPictureEl.querySelector('.cancel');
+  document.removeEventListener('keydown', onEscapeClose);
+  closeEl.removeEventListener('click', close);
+}
+
+function onEscapeClose(evt) {
+  if (isEscapeKey(evt)) {
+    close();
+  }
+}
+
 export function initBigPictureModal(postData) {
-  const onPostClickOpenModal = (evt) => {
-    const currentThumbnailEl = evt.target.closest('.picture');
-
-    if (!currentThumbnailEl) {
-      return;
-    }
-
-    const pictureId = currentThumbnailEl.dataset.pictureId;
-
-    const currentThumbnailData = postData.find((thumbnail) => thumbnail.id === Number(pictureId));
-
-    if (currentThumbnailEl) {
-      evt.preventDefault();
-      buildBigPicture(bigPictureEl, currentThumbnailData);
-
-      const closeEl = bigPictureEl.querySelector('.cancel');
-
-      bigPictureEl.classList.remove('hidden');
-      document.body.classList.add('modal-open');
-
-      closeEl.addEventListener('click', () => {
-        bigPictureEl.classList.add('hidden');
-        document.body.classList.remove('modal-open');
-      }, { once: true });
-    }
-  };
-
-  picturesContainerEl.addEventListener('click', onPostClickOpenModal);
+  picturesContainerEl.addEventListener('click', (evt) => {
+    show(evt, postData);
+  });
 }
